766e8649ed0061797e481795ec432f3f0049a2e3
Andréas Leroux <andreas.leroux@epitech.eu>
2025-02-14 11:49:52 +0100

    make youtube apiKey/on configurable

--- CustomApps/lyrics-plus/index.js
+++ CustomApps/lyrics-plus/index.js
@@ -93,6 +93,10 @@ const CONFIG = {
 	providersOrder: localStorage.getItem("lyrics-plus:services-order"),
 	modes: ["karaoke", "synced", "unsynced", "genius"],
 	locked: localStorage.getItem("lyrics-plus:lock-mode") || "-1",
+	youtube: {
+		on: getConfig("lyrics-plus:youtube:on", true),
+		apiKey: localStorage.getItem("lyrics-plus:youtube:apikey") || null,
+	}
 };
 
 try {
@@ -367,21 +371,26 @@ class LyricsContainer extends react.Component {
 				}
 			}
 		}
-		const baseURL = "https://www.googleapis.com/youtube/v3/search?maxResults=1&type=video&videoEmbeddable=true&";
 
-		const params = {
-			key: apiKey,
-			q: `${track.name} ${track.metadata.artist_name} video`,
-		};
+		const apiKey = CONFIG.youtube.apiKey;
+		if (apiKey && CONFIG.youtube.on) {
+			const baseURL = "https://www.googleapis.com/youtube/v3/search?maxResults=1&type=video&videoEmbeddable=true&";
 
-		const finalURL =
-			baseURL +
-			Object.keys(params)
-				.map((key) => `${key}=${encodeURIComponent(params[key])}`)
-				.join("&");
+			const params = {
+				key: apiKey,
+				q: `${track.name} ${track.metadata.artist_name} video`,
+			};
 
-		let result = await Spicetify.CosmosAsync.get(finalURL, null);
-		this.state.videoId = result.items?.[0]?.id?.videoId;
+			const finalURL =
+				baseURL +
+				Object.keys(params)
+					.map((key) => `${key}=${encodeURIComponent(params[key])}`)
+					.join("&");
+
+			let result = await Spicetify.CosmosAsync.get(finalURL, null);
+			this.state.videoId = result.items?.[0]?.id?.videoId;
+			// this.state.videoId = "1zN7J64IeBo";
+		}
 
 		this.lyricsSource(tempState, finalMode);
 
@@ -895,7 +904,7 @@ class LyricsContainer extends react.Component {
 					{
 						className: "lyrics-lyricsContainer-LyricsUnavailableMessage",
 					},
-					this.state.isLoading ? LoadingIcon : "(• _ • )"
+					this.state.isLoading ? LoadingIcon : (this.state.videoId ? "" : "(• _ • )")
 				)
 			);
 		}
